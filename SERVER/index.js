bce36db3296792f3e3a81573e65cc314c18a296a
bisVo159 <biswasanik159@gmail.com>
2024-09-12 15:57:05 +0530

    index

--- SERVER/index.js
+++ SERVER/index.js
@@ -24,7 +24,7 @@ database.conect()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials:true
 }))
 app.use(fileUpload({
